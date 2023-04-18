ddbf525892db150a2d7b0117a7b13445780191cc
izzoc <izzochristian662@gmail.com>
2023-04-18 17:41:13 -0400

    rebuilt dll to use any CPU

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,14 @@ import com.javonet.Javonet;
 import com.javonet.JavonetException;
 import com.javonet.JavonetFramework;
 
+/**
+ * A bit on how this works. The dll file in the .idea/csharp folder is the C# code compiled into the equivalent of a
+ * Java class library. Using the Javonet API, Java can translate the dll into something it can actually read and
+ * use. To use the methods you just specify the C# class with Javonet.getType, and then call the method with
+ * Javonet.invoke. We should only really need the one method for this, but if needed it should be easy going forward
+ * to use any code written in C#. I wrote the code in .NET core which is platform independent, so if you have a Mac
+ * there shouldn't be any need to even download Visual Studio, the dll file has everything in it.
+ */
 public class Test {
     public static void main(String[] args) throws JavonetException {
         Javonet.activate(Constants.email, Constants.APIkey, JavonetFramework.v40);
