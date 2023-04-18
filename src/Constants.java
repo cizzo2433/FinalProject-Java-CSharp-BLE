2802d0c4709ec175379715f98dd5275a6fa5a957
izzoc <izzochristian662@gmail.com>
2023-04-18 15:48:15 -0400

    more tests

--- src/Constants.java
+++ src/Constants.java
@@ -3,7 +3,7 @@
  */
 public class Constants {
     public static String email = "izzochristian662@gmail.com", APIkey = "Zt6s-Do2w-Fo98-q7L2-Ei27",
-    filePath = "C:\\Users\\izzoc\\source\\repos\\QuickBluetoothLE\\bin\\Debug\\QuickBlueToothLE.dll",
+    filePath = ".idea/csharp/QuickBluetoothLE.dll",
 
     // We should all decide on a beacon name so we can use a constant
     beaconName = "";
