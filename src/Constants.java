1761c3fae7d54b76b1297ee677f5a67e505e50ba
izzoc <izzochristian662@gmail.com>
2023-04-18 13:34:42 -0400

    testing updates

--- src/Constants.java
+++ src/Constants.java
@@ -3,5 +3,8 @@
  */
 public class Constants {
     public static String email = "izzochristian662@gmail.com", APIkey = "Zt6s-Do2w-Fo98-q7L2-Ei27",
-    filePath = "C:\\Users\\izzoc\\source\\repos\\QuickBluetoothLE\\bin\\Debug\\QuickBlueToothLE.dll";
+    filePath = "C:\\Users\\izzoc\\source\\repos\\QuickBluetoothLE\\bin\\Debug\\QuickBlueToothLE.dll",
+
+    // We should all decide on a beacon name so we can use a constant
+    beaconName = "";
 }
--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,8 @@ public class Test {
     public static void main(String[] args) throws JavonetException {
         Javonet.activate(Constants.email, Constants.APIkey, JavonetFramework.v40);
         Javonet.addReference(Constants.filePath);
+
+        // Will print message to console when signal is detected
         Javonet.getType("Program").invoke("CheckForBlueCharm");
     }
 }
