b0760c674e42f4f076459ed5d35d29fb05af287f
kyleruder <yellow.sand2973@ruders.net>
2023-04-20 15:43:23 -0400

    Updated Javadoc for WeatherAPI package

--- src/weatherAPI/LocationData.java
+++ src/weatherAPI/LocationData.java
@@ -1,5 +1,9 @@
 package weatherAPI;
 
+/**
+ * This class will hold location data returned by the OpenStreetMap API
+ * and make it available to be accessed by the Weather class.
+ */
 public class LocationData {
     double lat;
     double lon;
