15a720ead055f8cff74989581784adfaf1a2e47a
izzoc <izzochristian662@gmail.com>
2023-04-20 13:30:12 -0400

    added ref directory

--- src/Constants.java
+++ src/Constants.java
@@ -3,7 +3,7 @@
  */
 public class Constants {
     public static String email = "izzochristian662@gmail.com", APIkey = "Zt6s-Do2w-Fo98-q7L2-Ei27",
-    filePath = ".idea/csharp/QuickBluetoothLE.dll", googleAPIKey = "AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw",
+    filePath = "ref/csharp/QuickBluetoothLE.dll", googleAPIKey = "AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw",
 
     // We should all decide on a beacon name so we can use a constant
     beaconName = "";
