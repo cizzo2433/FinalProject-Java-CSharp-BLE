f2bd64999aa1384054ba257c855086ad1655107a
izzoc <izzochristian662@gmail.com>
2023-04-27 16:11:49 -0400

    added use of device location services

--- src/BeaconMain.java
+++ src/BeaconMain.java
@@ -6,27 +6,51 @@ import helpers.Constants;
 import helpers.SynthesizerV2;
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.advanced.AdvancedPlayer;
+import weatherAPI.Geocoder;
 import weatherAPI.Weather;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+/**
+ * Class that handles the combined functions of determining the users' location and determining the temperature
+ * and weather at that location. When a certain bluetooth signal is detected will use text to speech to say
+ * the weather data and suggest an activity based on that data.
+ */
 public class BeaconMain extends Activity_Wheel {
 
-    private final JavaWatcher jw;
+    private final JavaWatcher jw; // object that handles detection of BLE signals
 
+    /**
+     * Constructor, calls to Javonet API to initialize the use of C# class library
+     *
+     * @throws JavonetException
+     */
     public BeaconMain() throws JavonetException {
         Javonet.activate(Constants.email, Constants.APIkey, JavonetFramework.v40);
         Javonet.addReference(Constants.filePath);
         this.jw = new JavaWatcher();
     }
 
+    /**
+     * Listens for a specified bluetooth signal, and when detected, speaks a message with the temperature,
+     * weather conditions, and a suggested activity for the day
+     *
+     * @throws JavonetException
+     * @throws FileNotFoundException
+     */
     public void start() throws JavonetException, FileNotFoundException {
         boolean detected = false; // True once beacon signal detected
 
-        Weather.getWeather("19003");
-        System.out.println("Location Found");
+        //Weather.getWeather("19312");
+        //System.out.println("Location Found"); // For testing
 
+        // pulls coordinates directly from the devices location service
+        Double[] coordinates = Geocoder.geocode();
+
+        Weather.getWeather(coordinates[0], coordinates[1]);
+
+        // continue searching for BLE signal until detected
         while (!detected) {
             detected = jw.checkForBeacon();
 
@@ -48,6 +72,12 @@ public class BeaconMain extends Activity_Wheel {
         }
     }
 
+    /**
+     * Formats message to be grammatically correct. Based off possible return values of the OpenWeatherMap API
+     *
+     * @param condition the condition returned from the API
+     * @return a formatted message containing the current weather conditions
+     */
     private String formatMessage(String condition) {
 
         switch (condition) {
@@ -70,6 +100,13 @@ public class BeaconMain extends Activity_Wheel {
         return null;
     }
 
+    /**
+     * Converts a String message into an InputStream of MP3 data, which is returned in a Thread which will
+     * play the MP3 using JLayer. The Thread will not start until specified elsewhere in the program.
+     *
+     * @param text a String message
+     * @return a Thread
+     */
     private Thread textToSpeech(String text) {
 
         SynthesizerV2 synthesizer = new SynthesizerV2(Constants.googleAPIKey);
